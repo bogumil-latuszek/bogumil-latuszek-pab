ab7f92be4f8fbe3d67ac330c515fc3abaad6904c
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2022-05-04 21:30:54 +0200

    implemented calculator at /calc endpoint

--- index.ts
+++ index.ts
@@ -3,5 +3,37 @@ const app = express()
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
+app.get('/calc/', function (req, res) {
+    let operation = req.query.operation
+    let op
+    let result
+    let num1 = +req.query.num1
+    let num2 = +req.query.num2
+    switch(operation) {
+        case "dodaj":
+            op = '+'
+            result = num1 + num2
+            break
+      
+        case "usun":
+            op = '-'
+            result = num1 - num2
+            break
+            
+        case "podziel":
+            op = '/'
+            result = num1 / num2
+            break
+
+        case "pomnoz":
+            op = '*'
+            result = num1 * num2
+            break
+      
+        default:
+            res.send(`Unknown operation=${operation} - allowed: "dodaj", "usun", "podziel", "pomnoz"`)
+    }
+    res.send(`${num1} ${op} ${num2} = ${result}`)
+})
 app.listen(3000)
 
