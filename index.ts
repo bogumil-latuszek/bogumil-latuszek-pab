d15be1f68c30ad0c8fb52cd2aa3627c93a9715e4
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2022-05-04 21:50:47 +0200

    different way of passing GET params to calc
    see "Route parameters" at https://expressjs.com/en/guide/routing.html

--- index.ts
+++ index.ts
@@ -5,10 +5,42 @@ app.get('/', function (req, res) {
 })
 app.get('/calc/', function (req, res) {
     let operation = req.query.operation
-    let op
-    let result
     let num1 = +req.query.num1
     let num2 = +req.query.num2
+    let op
+    let result
+    switch(operation) {
+        case "dodaj":
+            op = '+'
+            result = num1 + num2
+            break
+      
+        case "usun":
+            op = '-'
+            result = num1 - num2
+            break
+            
+        case "podziel":
+            op = '/'
+            result = num1 / num2
+            break
+
+        case "pomnoz":
+            op = '*'
+            result = num1 * num2
+            break
+      
+        default:
+            res.send(`Unknown operation=${operation} - allowed: "dodaj", "usun", "podziel", "pomnoz"`)
+    }
+    res.send(`${num1} ${op} ${num2} = ${result}`)
+})
+app.get('/calc/:operation/:num1/:num2', function (req, res) {
+    let operation = req.params.operation
+    let num1 = +req.params.num1
+    let num2 = +req.params.num2
+    let op
+    let result
     switch(operation) {
         case "dodaj":
             op = '+'
@@ -36,4 +68,3 @@ app.get('/calc/', function (req, res) {
     res.send(`${num1} ${op} ${num2} = ${result}`)
 })
 app.listen(3000)
-
