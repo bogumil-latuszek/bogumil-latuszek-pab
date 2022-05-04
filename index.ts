6ad3f015c4a687f3233b4f79ab01a53948be1ced
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2022-05-04 20:40:36 +0200

    remove index.js
    JavaScript is generated from TypeScript via npx tsc index.ts

--- index.ts
+++ index.ts
@@ -1,17 +1,7 @@
-import express from 'express'
-import { Request, Response } from 'express'
-
+const express = require('express')
 const app = express()
-
-app.use(express.json())
-
-app.get('/', function (req: Request, res: Response) {
-    res.send('GET Hello World')
+app.get('/', function (req, res) {
+    res.send('Hello World')
 })
-app.post('/', function (req: Request, res: Response) {
-    console.log(req.body) // e.x. req.body.title 
-    res.status(200).send('POST Hello World')
-})
-
 app.listen(3000)
 
