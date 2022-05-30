1372a6cf2422d322d1a68b0237b9a8b3fa91b493
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2022-05-30 19:29:29 +0200

    refactor tags.ts - exchange Map<number, Tag> with InMemoryTags

--- projekt2/src/tags.ts
+++ projekt2/src/tags.ts
@@ -1,16 +1,14 @@
 import * as core from 'express-serve-static-core';
 import {Request, Response} from 'express';
 import {Tag} from './model';
-import Unique_id_generator from './id';
+import { ITagsAccess, InMemoryTags } from './data_storage'
 
-let tags: Map<number, Tag> = new Map<number, Tag>(); 
-let gen: Unique_id_generator = new Unique_id_generator();
+let tags: ITagsAccess = new InMemoryTags();
 
 export default function register_tag_routes(app: core.Express) {
     app.get('/tags/', (req: Request, res: Response) => {
-        if (tags.size > 0) {
-            let tag_table: Tag[] = [];
-            tags.forEach((tag: Tag) => tag_table.push(tag));
+        if (tags.getTagsCount() > 0) {
+            let tag_table: Tag[] = tags.getAllTags();
             res.status(200).send(tag_table);
         }
         else {
@@ -20,8 +18,8 @@ export default function register_tag_routes(app: core.Express) {
     
     app.get('/tag/:id', (req: Request, res: Response) => {
         let id = +req.params.id
-        if (tags.has(id)) {
-            let tag = tags.get(id);
+        if (tags.hasTag(id)) {
+            let tag = tags.getTag(id);
             res.status(200).send(tag)
         }
         else {
@@ -38,16 +36,13 @@ export default function register_tag_routes(app: core.Express) {
         } 
         else {
             let tag: Tag = req.body;
-            tag.name = tag.name.toLowerCase();
-            let tag_id = find_tag_id(tag.name);
+            let tag_id = tags.findTagId(tag.name);
             if (tag_id != undefined) {
                 res.status(200).send({'id' : tag_id})
             }
             else {
-                let id: number = gen.generate_unique_id(tags);
-                tag.id = id;
-                tags.set(id, tag);
-                res.status(201).send({'id': id })
+                tag = tags.addTag(tag);
+                res.status(201).send({'id': tag.id })
             }
         }
     })
@@ -55,60 +50,47 @@ export default function register_tag_routes(app: core.Express) {
     app.put('/tag/:id', (req: Request, res: Response) => {
         let tag: Tag = req.body;
         let id = +req.params.id;
-        if (!tags.has(id)) {
+        if (!tags.hasTag(id)) {
             res.status(404).send({'err': 'tag with this id not found'})
         }
-        let tag_id = find_tag_id(tag.name);
+        let tag_id = tags.findTagId(tag.name);
         if (tag_id != undefined) {
             res.status(400).send({'error' : `'${tag.name}' already used by id:'${tag_id}'`})
         }
         else {
             tag.id = id;
-            tags.set(id, tag);
+            tags.updateTag(tag);
             res.status(204).send({'id': id})
         }
     })
 
     app.delete('/tag/:id', (req: Request, res: Response) => {
         let id = +req.params.id;
-        if (!tags.has(id)) {
+        if (!tags.hasTag(id)) {
             res.status(404).send({'err': 'tag with this id not found'})
         }
-        tags.delete(id);
+        tags.deleteTag(id);
         res.status(204).send();
     })
 }
 
 export function process_tags(new_or_existing: Tag[]): Tag[] {
-    let new_tags: Tag[] = [];
+    let tags_set: Tag[] = [];
     new_or_existing.forEach((tag: Tag) => {
-        let new_tag: Tag;
-        new_tag = process_single_tag(tag.name);
-        new_tags.push(new_tag);
+        let full_tag: Tag = process_single_tag(tag.name);
+        tags_set.push(full_tag);
     })
-    return new_tags;
+    return tags_set;
 }
 
 function process_single_tag(tag_name: string): Tag {
-    let tag: Tag = {name: tag_name.toLowerCase()};
-    let tag_id = find_tag_id(tag.name);
+    let tag: Tag = {name: tag_name};
+    let tag_id = tags.findTagId(tag.name);
     if (tag_id != undefined) {
         tag.id = tag_id;
     }
     else {
-        let id: number = gen.generate_unique_id(tags);
-        tag.id = id;
-        tags.set(id, tag);
+        tags.addTag(tag);
     }
     return tag;
-}
-
-function find_tag_id(name: string) {
-    let tag_id : number|undefined = undefined;
-    tags.forEach((tag: Tag) => { 
-        if (tag.name == name) {
-            tag_id = tag.id;
-        }
-    })
-    return tag_id;
 }
